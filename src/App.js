b2417b2ec89d7f4cc97d336fa65ffcc0e09ed972
heheboizzzz <mrtuan154@gmail.com>
2023-05-25 16:40:56 +0700

    how to use Effect & Context Hook

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Main from './Components/Main';
 import Footer from './Components/Footer/Footer';
